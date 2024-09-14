65907ec7531290ce8622caf02436f17fe8de1901
Hugo <smahlij@gmail.com>
2024-09-13 22:50:18 -0300

    implementacion de hook personalizado en login

---
+++ src/hooks/useForm.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+function useForm(initialValues, validate) {
+  const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (callback) => (e) => {
+    e.preventDefault();
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      callback();
+    }
+  };
+
+  return {
+    values,
+    errors,
+    handleChange,
+    handleSubmit,
+  };
+}
+
+export default useForm;
