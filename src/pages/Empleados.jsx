3989a063f79fa681c0e33e89e817395b73143ece
Hugo <smahlij@gmail.com>
2024-09-14 18:01:52 -0300

    implementacion para agregado de empleados

--- src/pages/Empleados.jsx
+++ src/pages/Empleados.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import EmpleadosForm from "../components/EmpleadosForm";
 
 
 function Empleados () {
@@ -7,24 +8,37 @@ function Empleados () {
     const [empleados, setEmpleados] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [formValues, setFormValues] = useState({ nombre: '', rol: '' });
 
     // Funcion para obtener los empleados desde el servidor
     const fetchEmpleados = async () => {
         try {
             const response = await axios.get('http://localhost:3001/empleados'); // URL del json-server
-            if (!response.ok) {
-                throw new Error('Error al obtener empleados');
-            }
-            const data = await response.json();
-            setEmpleados(data);
+            setEmpleados(response.data);
             setLoading(false);
         } catch (err) {
-            setError(err.message);
+            console.error('Error al obtener empleados', err.response || err.message);
+            setError('Error al obtener empleados');
             setLoading(false);
         }
     };
 
-    
+    // Funcion para agregar un nuevo empleado
+    const agregarEmpleado = async (values) => {
+        try {
+            const response = await axios.post('http://localhost:3001/empleados', {
+                name: values.nombre,
+                role: values.rol
+            });
+            setEmpleados([...empleados, response.data]); // aqui se añade un nuevo empleado a la lista
+            setFormValues({nombre: '', rol: ''})
+        } catch (err) {
+            console.error('Error al agregar el empleado', err.response || err.message);
+            setError('Error al agregar el empleado')
+        }
+    }
+
+    // Obtener empleados al montar el componente
     useEffect(() => {
         fetchEmpleados()
     }, []);
@@ -43,6 +57,12 @@ function Empleados () {
                     </li>
                 ))}
             </ul>
+
+            {/* Formulario para agregar un nuevo empleado */}
+            <h3>Agregar nuevo empleado</h3>
+            <EmpleadosForm
+                onSubmit={agregarEmpleado}
+            />
         </div>
     )
 }
