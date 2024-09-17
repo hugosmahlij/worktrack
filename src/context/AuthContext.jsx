5379864ddff182a505daa3a66e07202b7871ce19
Hugo <smahlij@gmail.com>
2024-09-17 11:51:57 -0300

    agregado de funcionalidad para ingresar con usuarios de la base de datos

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AuthContext = createContext();
 
@@ -7,11 +8,25 @@ export function AuthProvider ({ children }) {
     const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem("token"));
     const navigate = useNavigate();
 
-    const login = () => {
-        // Simula un token que se guarda en el localStorage
-        const token = 'tokenficticio1234';
-        localStorage.setItem("token", token)
-        setIsAuthenticated(true);
+    const login = async (username, password) => {
+        try {
+            const response = await axios.get('http://localhost:3001/usuarios');
+            const usuarios = response.data;
+
+            const usuarioEncontrado = usuarios.find(user => user.username === username && user.password === password);
+
+            if (usuarioEncontrado) {
+                // Simula un token que se guarda en el localStorage
+                const token = 'tokenficticio1234';
+                localStorage.setItem("token", token)
+                setIsAuthenticated(true);
+                navigate('/')
+            } else {
+                alert ("Usuario o contraseña incorrectos")
+            }
+        } catch (error) {
+            console.error('Error al intentar iniciar sesión: ', error)
+        }
     }
 
     const logout = () => {
